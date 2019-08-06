885f31fff5889f68cb8602526a0db3b762393ecb
taichi <taichi730@gmail.com>
2019-08-06 18:22:56 +0900

    marked RWE and RWL as known access

--- rggen_ral_field.svh
+++ rggen_ral_field.svh
@@ -1,1 +1,19 @@
-typedef uvm_reg_field rggen_ral_field;
+class rggen_ral_field extends uvm_reg_field;
+  function new(string name = "rggen_ral_field");
+    super.new(name);
+  endfunction
+
+  virtual function bit is_known_access(uvm_reg_map map = null);
+    if (super.is_known_access(map)) begin
+      return 1;
+    end
+    else begin
+      string  access  = get_access(map);
+      case (access)
+        "RWE":    return 1;
+        "RWL":    return 1;
+        default:  return 0;
+      endcase
+    end
+  endfunction
+endclass
